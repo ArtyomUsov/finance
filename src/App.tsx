b95df071e89f05eb6b75fd4278d3265c3c61416f
Usov_Artem <artemka063@gmail.com>
2024-02-05 16:41:30 +0400

    feat: add router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import BalancePage from "./pages/BalancePage";
+import IncomePage from "./pages/IncomePage";
+import ExpensesPage from "./pages/ExpensesPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-
+    <Header/>
+    <Routes>
+      <Route path="/finance" element={<BalancePage/>}/>
+      <Route path="/income" element={<IncomePage/>}/>
+      <Route path="/expenses" element={<ExpensesPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
